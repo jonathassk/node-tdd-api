2aa3667fb272eb5bb96fd02c182a77e085e6840b
Jonathas <jonathas09@gmail.com>
2020-04-30 02:14:29 -0300

    confirmando que loginRouter retorna 500 caso um useCase de autenticação é invalido

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -77,4 +77,28 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+  test('should return 500 if no authUseCase is provided.', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'email@email.com',
+        password: 'password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('should return 500 if no authUseCase is Provided', () => {
+    const sut = new LoginRouter({})
+    const httpRequest = {
+      body: {
+        email: 'email@email.com',
+        password: 'password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })
