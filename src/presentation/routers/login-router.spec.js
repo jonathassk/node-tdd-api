6b60c5a4955112c13f3800a33c3efd812f6f78ae
Jonathas <jonathas09@gmail.com>
2020-05-02 02:19:34 -0300

    organizando arquivos de erros para uma pasta separada dos helpers

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,8 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('../helpers/missing-param-error')
-const UnauthorizedError = require('../helpers/unauthorized-error')
-const InvalidParamError = require('../helpers/invalid-param-error')
-const ServerError = require('../helpers/server-error')
+const MissingParamError = require('../errors/missing-param-error')
+const { UnauthorizedError } = require('../errors/index')
+const InvalidParamError = require('../errors/invalid-param-error')
+const ServerError = require('../errors/server-error')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
