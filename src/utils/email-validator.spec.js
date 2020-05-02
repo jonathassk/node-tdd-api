02c2fc6742f8073b80baf58c3bab043079e0229c
Jonathas <jonathas09@gmail.com>
2020-05-02 02:47:30 -0300

    confirmando que email recebido do validator é igual o da requisiçaõ

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,17 +6,27 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should return true if email is valid.', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator return false', () => {
     Validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalidEmail@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('should return correct email', () => {
+    const sut = makeSut()
+    sut.isValid('email@email.com')
+    expect(Validator.email).toEqual('email@email.com')
   })
 })
