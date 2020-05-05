ec6523fe2adf812c0a9cd90541ce62e78582410a
Jonathas <jonathas09@gmail.com>
2020-05-04 23:21:36 -0300

    teste missingParamError caso não tenha senha

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,12 @@
 const { MissingParamError } = require('../../presentation/errors/index')
 
 class AuthUseCase {
-  async Auth (email) {
+  async Auth (email, password) {
     if (!email) {
       throw new MissingParamError('Email')
+    }
+    if (!password) {
+      throw new MissingParamError('Password')
     }
   }
 }
@@ -13,5 +16,11 @@ describe('Auth useCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.Auth()
     expect(promise).rejects.toThrow(new MissingParamError('Email'))
+  })
+
+  test('Should throw if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.Auth('email@email.com')
+    expect(promise).rejects.toThrow(new MissingParamError('Password'))
   })
 })
