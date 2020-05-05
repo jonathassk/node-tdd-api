584862ef5f0c03c7285c17911da8ef607b121e71
Jonathas <jonathas09@gmail.com>
2020-05-04 23:08:24 -0300

    confirmando que authUseCase dara missing param error caso o email não seja recebido

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,9 @@
+const { MissingParamError } = require('../../presentation/errors/index')
+
 class AuthUseCase {
   async Auth (email) {
     if (!email) {
-      throw new Error()
+      throw new MissingParamError('Email')
     }
   }
 }
@@ -10,6 +12,6 @@ describe('Auth useCase', () => {
   test('Should throw if no email is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.Auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamError('Email'))
   })
 })
