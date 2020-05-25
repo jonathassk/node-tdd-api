b521a50b23d2aba3f15a0bca8b390f5816ff6cdb
jonathas fonseca <jonathas09@gmail.com>
2020-05-25 16:17:58 -0300

    criação metodo makeSut no teste do encriptador

--- src/presentation/helpers/encrypter.spec.js
+++ src/presentation/helpers/encrypter.spec.js
@@ -7,22 +7,26 @@ class Encrypter {
   }
 }
 
+const makeSut = () => {
+  return new Encrypter()
+}
+
 describe('Encrypter', () => {
   test('Should return true if bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('value', 'hash')
     expect(isValid).toBe(true)
   })
 
   test('Should return true if bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('value', 'hash')
     expect(isValid).toBe(false)
   })
 
   test('bcrypt should recieve correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     await sut.compare('value', 'hash')
     expect(bcrypt.value).toBe('value')
     expect(bcrypt.hash).toBe('hash')
