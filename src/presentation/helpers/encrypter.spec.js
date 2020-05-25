7a6d6c1e7f33ed8fedd0ac8c408fc12b8816fe82
jonathas fonseca <jonathas09@gmail.com>
2020-05-25 16:45:14 -0300

    correção ortografica

--- src/presentation/helpers/encrypter.spec.js
+++ src/presentation/helpers/encrypter.spec.js
@@ -20,24 +20,24 @@ describe('Encrypter', () => {
     expect(isValid).toBe(false)
   })
 
-  test('bcrypt should recieve correct values', async () => {
+  test('Bcrypt should recieve correct values', async () => {
     const sut = makeSut()
     await sut.compare('value', 'hash')
     expect(bcrypt.value).toBe('value')
     expect(bcrypt.hash).toBe('hash')
   })
 
-  test('Should throw if values are not prvided', async () => {
+  test('Should throw if values are not provided', async () => {
     const sut = makeSut()
     expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
   })
 
-  test('Should throw if values are not prvided', async () => {
+  test('Should throw if values are not provided', async () => {
     const sut = makeSut()
     expect(sut.compare('value')).rejects.toThrow(new MissingParamError('hash'))
   })
 
-  test('Should throw if values are not prvided', async () => {
+  test('Should throw if values are not provided', async () => {
     const sut = makeSut()
     expect(sut.compare('', 'hash')).rejects.toThrow(new MissingParamError('value'))
   })
