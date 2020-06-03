642fa1eedc9c08f56d937b8155db6a8a02770c84
jonathas fonseca <jonathas09@gmail.com>
2020-06-03 16:14:55 -0300

    confirmando que é recebido o token quando o jwt retorna o token

--- src/presentation/helpers/token-generator.spec.js
+++ src/presentation/helpers/token-generator.spec.js
@@ -1,13 +1,22 @@
+const jwt = require('jsonwebtoken')
+
 class TokenGenerator {
   async Generate (id) {
-    return null
+    return jwt.sign(id, 'secret')
   }
 }
 
 describe('token generator', () => {
   test('Should return null if JWT returns null.', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.Generate('id')
     expect(token).toBeNull()
+  })
+
+  test('Should return a token if JWT returns token.', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.Generate('id')
+    expect(token).toBe(jwt.token)
   })
 })
