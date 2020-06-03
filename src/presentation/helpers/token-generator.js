de6b1741e723b7bbbaf901ed7ea837dfaefe7c52
jonathas fonseca <jonathas09@gmail.com>
2020-06-03 16:32:04 -0300

    token gerenator movido para seu proprio arquivo

---
+++ src/presentation/helpers/token-generator.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const { MissingParamError } = require('../errors/index')
+class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
+  async Generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+    return jwt.sign(id, this.secret)
+  }
+}
+
+module.exports = TokenGenerator
