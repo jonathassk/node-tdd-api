e190bf2a4e2e520cd132cb8f94b52058f57141bc
jonathas fonseca <jonathas09@gmail.com>
2020-06-03 16:23:07 -0300

    testando se jwt esta recebendo valores corretos

--- src/presentation/helpers/token-generator.spec.js
+++ src/presentation/helpers/token-generator.spec.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken')
 
 class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
   async Generate (id) {
-    return jwt.sign(id, 'secret')
+    return jwt.sign(id, this.secret)
   }
 }
 
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator('secret')
 }
 
 describe('token generator', () => {
@@ -22,5 +26,12 @@ describe('token generator', () => {
     const sut = makeSut()
     const token = await sut.Generate('id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct values.', async () => {
+    const sut = makeSut()
+    await sut.Generate('id')
+    expect(jwt.id).toBe('id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
