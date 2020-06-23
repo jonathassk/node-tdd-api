088141c149b82f4e21b938c2ea4aa1b7eab7edfb
jonathas fonseca <jonathas09@gmail.com>
2020-06-23 00:30:13 -0300

    configurando routes iniciais

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const App = express()
 const SetupApp = require('./setup')
+const Routes = require('./routes')
 
 SetupApp(App)
+Routes(App)
 
 module.exports = App
