e379feef09a5fa4708b18e5b6e0d5c41c5254610
jonathas fonseca <jonathas09@gmail.com>
2020-06-23 00:10:00 -0300

    conectando ao bd antes de ligar o server

--- src/main/index.js
+++ src/main/index.js
@@ -5,7 +5,7 @@ const env = require('../main/config/env')
 mongoHelper.connect(env.mongoUrl)
   .then(() => {
     App.listen(3000, () => {
-      console.log('server running')
+      console.log('server running!')
     })
   })
   .catch(console.error)
